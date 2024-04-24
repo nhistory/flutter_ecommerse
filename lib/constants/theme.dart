dbf435c130571d8419216ce604c45d740be02b03
Sehwan <sehwan.lee.dev@gmail.com>
2024-04-24 16:50:29 -0300

    Implemented login input form with visibility btn

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 ThemeData themeData = ThemeData(
   scaffoldBackgroundColor: Colors.white,
+  inputDecorationTheme: InputDecorationTheme(
+    border: outlineInputBorder,
+    errorBorder: outlineInputBorder,
+    enabledBorder: outlineInputBorder,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+    focusedBorder: outlineInputBorder,
+    disabledBorder: outlineInputBorder,
+  ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
         backgroundColor: Colors.red,
@@ -12,5 +21,11 @@ ThemeData themeData = ThemeData(
           fontSize: 18.0,
           fontWeight: FontWeight.bold,
         )),
+  ),
+);
+
+OutlineInputBorder outlineInputBorder = const OutlineInputBorder(
+  borderSide: BorderSide(
+    color: Colors.grey,
   ),
 );
--- lib/screens/auth_ui/login/login.dart
+++ lib/screens/auth_ui/login/login.dart
@@ -1,28 +1,78 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerse/widgets/top_titles/top_titles.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isShowPassword = true;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(30.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
+            const SizedBox(
               height: kToolbarHeight,
             ),
-            Padding(
+            const Padding(
               padding: EdgeInsets.only(left: 10.0),
               child: BackButton(),
             ),
-            SizedBox(
+            const SizedBox(
               height: 12.0,
             ),
-            TopTitles(title: "Login", subtitle: "Welcome back to SH ecommerce")
+            const TopTitles(
+                title: "Login", subtitle: "Welcome back to SH ecommerce"),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "E-mail",
+                  prefixIcon: Icon(
+                    Icons.email_outlined,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: TextFormField(
+                obscureText: isShowPassword,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: const Icon(
+                    Icons.key,
+                  ),
+                  suffixIcon: CupertinoButton(
+                    onPressed: () {
+                      setState(() {
+                        isShowPassword = !isShowPassword;
+                        // print(isShowPassword);
+                      });
+                    },
+                    padding: EdgeInsets.zero,
+                    child: const Icon(
+                      Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
